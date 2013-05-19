9f003312e279c61353887ceda5e49351130faeab
CHANDLER HARBEN <chandlerharben@new-host-2.home>
2013-05-18 22:46:13 -0400

    update

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('CLC 23\n');
+  res.end('CLC 21\n');
 }).listen(process.env.PORT || 1337, '127.0.0.1');
 console.log('Server running :1337');
